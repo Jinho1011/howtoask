1133e87b42bca17b56d2de3c67ee8b4a0bb5a038
Jeon <gino9940@gmail.com>
2023-07-28 14:18:27 +0900

    build: add i18n settings

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  i18n: {
+    locales: ['en', 'ko'],
+    defaultLocale: 'en',
+    localeDetection: false,
+  },
+};
 
 module.exports = nextConfig;
